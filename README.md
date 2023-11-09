ba4cfe2646ba7e6299343297f94b88894f53948e
Farhan Khan <fkhan41@stuy.edu>
2023-11-09 00:09:44 -0500

    started prokect

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # 03_Dice_Project
- Project Name: Going to Boston
+ Project Name: Going to Boston \n
  Partners: Farhan Khan & Sadat Ahmed
--- js/script.js
+++ js/script.js
@@ -1,1 +1,3 @@
-
+document.querySelector(".start").addEventListener("click", function() {
+    window.location.replace("file:///Users/farhankhan/Desktop/softdev/03_Dice_Project/game.html")
+})
