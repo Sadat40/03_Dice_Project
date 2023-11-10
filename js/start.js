8db0bfe8c6211e4aa8cab03670855c379899956e
Sadat40 <sahmed40@stuy.edu>
2023-11-10 14:26:01 -0500

    redirect works

--- js/start.js
+++ js/start.js
@@ -1,13 +1,17 @@
 document.querySelector(".start").addEventListener("click", function () {
-     if (document.querySelector(".rounds").textContent  == "") { // if empty
+     if (document.querySelector(".rounds").value  == "") { // if empty
         document.querySelector(".msg").textContent = "Please enter an odd number"
      }
 
      else {
-        let val = Number(document.querySelector(".start").value)
-        if (val % 2 == 1) // if odd, then continue to game page
-            window.location.replace("game.html")
-     }
-     
-}
+        let val = Number(document.querySelector(".rounds").value)
+        if (val%2===1){
+         // If it's a valid number and an odd number, then continue to the game page
+         document.querySelector(".msg").value = ""
+         window.location.replace("game.html");
+     } else {
+         // Handle the case where the content is not a valid odd number
+         document.querySelector(".msg").textContent = "Please enter a valid odd number";
+     } 
+}}
 );
