f03a82c0c2333d537d231d9fb8d419403dbbf88b
Sadat40 <sahmed40@stuy.edu>
2023-11-10 14:51:00 -0500

    pushing

--- js/game.js
+++ js/game.js
@@ -1,3 +1,18 @@
 document.querySelector(".home").addEventListener("click", function() {
     window.location.replace("start.html")
 });
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+
+    document.querySelector(".roll1").addEventListener("click", function() {
+    let roll = getRandomInt(1,6)
+    document.querySelector(".player1").textContent = `Player 1 rolled: ${roll}`
+})
+
+document.querySelector(".roll2").addEventListener("click", function() {
+    let roll = getRandomInt(1,6)
+    document.querySelector(".player2").textContent = `Player 2 rolled: ${roll}`
+})
