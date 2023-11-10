f5fa4f0b876aaf81f4a8f1a2005130c7624ed324
Farhan Khan <fkhan41@stuy.edu>
2023-11-10 01:11:22 -0500

    pushing changes

--- js/start.js
+++ js/start.js
@@ -1,10 +1,13 @@
 document.querySelector(".start").addEventListener("click", function () {
-    //  if (document.querySelector(".rounds").value!=null){
-    //      if (Number(document.querySelector(".start").value)%2===1){
-            window.location.replace( "game.html")
-    //      }
-    //      else{
+     if (document.querySelector(".rounds").textContent  == "") { // if empty
+        document.querySelector(".msg").textContent = "Please enter an odd number"
+     }
 
-    //      }
-    //  }
-});
+     else {
+        let val = Number(document.querySelector(".start").value)
+        if (val % 2 == 1) // if odd, then continue to game page
+            window.location.replace("game.html")
+     }
+     
+}
+);
