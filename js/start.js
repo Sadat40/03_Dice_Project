bd6a5e4e764d15ef2ed2adc136cde95b5e89559c
Farhan Khan <fkhan41@stuy.edu>
2023-11-10 00:28:23 -0500

    pushing changes

---
+++ js/start.js
@@ -0,0 +1,10 @@
+document.querySelector(".start").addEventListener("click", function () {
+    //  if (document.querySelector(".rounds").value!=null){
+    //      if (Number(document.querySelector(".start").value)%2===1){
+            window.location.replace( "game.html")
+    //      }
+    //      else{
+
+    //      }
+    //  }
+});
